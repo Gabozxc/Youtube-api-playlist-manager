0a5b31cb1c81a3d1d7bfdb1dc1ed978940e062b4
Gabriel <gabozxc@gmail.com>
2022-02-22 15:58:01 -0400

    prueba peticion
    prueba mejorar peticion

--- components/PreviewVideo.js
+++ components/PreviewVideo.js
@@ -1,10 +1,9 @@
 import Image from "next/image";
 
-const PreviewVideo = ({ data, title, url }) => {
+const PreviewVideo = ({ title, url }) => {
   return (
     <div className="w-[250px] m-5 hover:bg-gray-200 p-2 flex flex-col rounded-lg">
       <h2 className="mb-4 mt-4 font-bold">{title}</h2>
-      {console.log(url)}
       {url ? (
         <Image
           alt={title}
--- pages/playlist/[id].js
+++ pages/playlist/[id].js
@@ -11,35 +11,37 @@ export default function Playlist() {
   const [loading, setLoading] = useState(true);
   const [subsList, setSubsList] = useState([]);
 
-  useEffect(() => {
-    const getYTData = async () => {
-      if (loading) {
-        try {
-          const { data } = await axios.post("/api/getPlaylistSongs", {
-            withCredentials: true,
-            id,
-          });
-          setLoading(false);
-          setSubsList(data);
-          console.log(data)
-        } catch (err) {
-          console.log(err);
-        }
+  const getYTData = async () => {
+    if (loading) {
+      try {
+        const { data } = await axios.post("/api/getPlaylistSongs", {
+          withCredentials: true,
+          id,
+        });
+        setLoading(false);
+        setSubsList(data);
+        console.log(data)
+      } catch (err) {
+        console.log(err);
       }
-    };
-    getYTData();
+    }
+  };
+  useEffect(() => {
+    if(id){
+      getYTData();
+    }
   }, [loading, id]);
 
   return (
     <Layout>
       <div className="pl-10 text-center">
+        <button className="bg-blue-500 border-solid hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer" onClick={() => setLoading(true)}>Refresh</button>
         <section className="flex flex-row flex-wrap justify-start">
           {subsList?.map((sub) => (
             <PreviewVideo
               key={sub.id}
               title={sub.snippet.title}
               url={sub.snippet?.thumbnails?.high?.url}
-              data={sub}
             />
           ))}
           {loading && <p>Loading...</p>}
