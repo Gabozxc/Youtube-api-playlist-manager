7a8fecdc44d48d7c8483fd5b226ef6dd9bd32b19
Gabriel <gabozxc@gmail.com>
2022-02-25 15:33:59 -0400

    prueba cerrar sesion
    asd

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,25 @@
-import { signIn, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
+
 import { Layout, PlayListHome } from "../components/root";
 
 export default function Home() {
+
   const { data: session } = useSession();
+
+  //get expire time from session
+  const expireTime = session ? session.expires : null;
+
+  //get times and signout
+  const time = expireTime ? new Date(expireTime) : null;
+  const timeNow = new Date();
+
+  //get time difference
+  const timeDiff = time ? timeNow.getTime() - time.getTime() : null;
+
+  //if time difference is greater than 0, signout and redirect to signin
+  if (timeDiff > 0) {
+    signOut({ returnTo: "/" });
+  }
 
   return (
     <Layout>
